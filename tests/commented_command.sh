55f93dc2a63d376507189e9566f666c7431f1a42
Lisaveta <lmish001@ucr.edu>
2017-03-04 05:46:57 +0000

    commented test cases

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -1,98 +1,71 @@
 #!/bin/bash
-# Use tests/commented_command.sh while in rshell directory to run tests 
+# Use tests/commented_test.sh while in rshell directory to run tests 
 
-echo "Testing ouput for commented commands:"
+echo "Testing ouput with comments:"
 echo
 
 # The last prompt "username@machinename$" is just a text prompt. rshell will break after it is printed.
 # No command will be executed between the last prompt to "Case [#] Test Complete"
-echo "Case 1"
-USERINPUT="#Do not ouput this # echo \"See what happens\""
+CASENUM=0
+RED='\033[0;91m'
+REG='\033[0m'
+
+printf "${RED}Testing comments after parentheses${REG} \n"
+
+((CASENUM++));
+echo "Case $CASENUM"
+USERINPUT="( echo a && echo b ) && (echo c && echo d) #hello"
 echo "Input: $USERINPUT"
 echo "Output:"
 echo
 bin/rshell <<< $USERINPUT
 echo
-echo "Case 1 Test Complete"
+echo "Case $CASENUM Test Complete"
 echo
 
-echo "Case 2"
-USERINPUT="echo hello && echo hi there # mkdir case4_mult_test"
+((CASENUM++));
+echo "Case $CASENUM"
+USERINPUT="((echo a && echo b) && (echo c && echo d)) #echo \"this is a comment\" && ls"
 echo "Input: $USERINPUT"
 echo "Output:"
 echo
 bin/rshell <<< $USERINPUT
 echo
-echo "Case 2 Test Complete"
+echo "Case $CASENUM Test Complete"
 echo
 
-echo "Case 3"
-USERINPUT="echo this; # echo is && echo a || echo test"
+printf "${RED}Syntax errors${REG} \n"
+
+((CASENUM++))
+echo "Case $CASENUM"
+USERINPUT="(echo a && echo b # stop)"
 echo "Input: $USERINPUT"
 echo "Output:"
 echo
 bin/rshell <<< $USERINPUT
 echo
-echo "Case 3 Test Complete"
+echo "Case $CASENUM Test Complete"
 echo
 
-echo "Case 4"
-USERINPUT="#echo a; echo b # echo c; echo d"
+((CASENUM++));
+echo "Case $CASENUM"
+USERINPUT="(echo a # stop) && echo b)"
 echo "Input: $USERINPUT"
 echo "Output:"
 echo
 bin/rshell <<< $USERINPUT
 echo
-echo "Case 4 Test Complete"
+echo "Case $CASENUM Test Complete"
 echo
 
-echo "Case 5"
-USERINPUT="echo hello ; ls #do not print"
+((CASENUM++));
+echo "Case $CASENUM"
+USERINPUT="((echo a # stop) && echo b)"
 echo "Input: $USERINPUT"
 echo "Output:"
 echo
 bin/rshell <<< $USERINPUT
 echo
-echo "Case 5 Test Complete"
-echo
-
-echo "Case 6"
-USERINPUT="echo \"hello#comment#\""
-echo "Input: $USERINPUT"
-echo "Output:"
-echo
-bin/rshell <<< $USERINPUT
-echo
-echo "Case 6 Test Complete"
-echo
-
-echo "Case 7"
-USERINPUT="#"
-echo "Input: $USERINPUT"
-echo "Output:"
-echo
-bin/rshell <<< $USERINPUT
-echo
-echo "Case 7 Test Complete"
-echo
-
-echo "Case 8"
-USERINPUT="mkdir case7_commented_test && rm -rf case7_commented_test && # echo \" what happens\""
-echo "Input: $USERINPUT"
-echo "Output:"
-echo
-bin/rshell <<< $USERINPUT
-echo
-echo "Case 8 Test Complete"
-echo
-
-echo "Case 9"
-USERINPUT="rm -rf case8_commented_test ; ls  ||# echo \" what happens\""
-echo "Input: $USERINPUT"
-echo "Output:"
-echo
-bin/rshell <<< $USERINPUT
-echo
-echo "Case 9 Test Complete"
+echo "Case $CASENUM Test Complete"
 echo
 
